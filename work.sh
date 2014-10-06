1126c5770989408830775ed1d6390eada657f7bf
Tehforsch <Tonipeter92@googlemail.com>
2014-10-06 20:25:37 +0200

    Work.sh works again ;)

--- work.sh
+++ work.sh
@@ -1,2 +1,2 @@
 # WE META NOW
-when-changed *.py -c "./killAndStart.sh"
+when-changed dependencies.py config.py dependency.py executor.py fileState.py main.py makeRunView.py observer.py tools.py -c "sh ./killAndStart.sh"
