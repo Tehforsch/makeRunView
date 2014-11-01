00c7aa2d1d8d55137f85006cf6faa2d16f053d75
Tehforsch <Tonipeter92@googlemail.com>
2014-11-01 16:18:18 +0100

    Accounted for new files in kill script

--- killAndStart.sh
+++ killAndStart.sh
@@ -2,4 +2,4 @@ clear
 # Check if the python script was already running, terminate it
 processId=$(ps axf | ack python3 | ack main.py | ack example | ack -v ack | ack  "^([\s]*[\d]*)"  --output=\$1)
 kill $processId > /dev/null 2>&1
-python3 -B main.py example & 
+python3 -B main.py -v example & 
