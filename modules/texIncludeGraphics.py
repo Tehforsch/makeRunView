0f6d95894701b1ff97a80445560655dd8c927cfe
Tehforsch <Tonipeter92@googlemail.com>
2014-11-15 12:53:21 +0100

    The include graphics module now checks for available files instead of just assuming png when a file is imported without ending

--- modules/texIncludeGraphics.py
+++ modules/texIncludeGraphics.py
@@ -1,4 +1,4 @@
-import tools
+import tools, os,logging
 from dependency import Dependency
 def check(f, lines):
     if f.fileType != "tex":
@@ -9,9 +9,19 @@ def check(f, lines):
     for l in lines:
         if "includegraphics" in l:
             filename = tools.charactersBetween(l, "{", "}")
-            if tools.getFileType(filename) is None:
-                # Lets try png i dont want to check how the file could be named
-                filename = filename + ".png"
+            if tools.getFileType(filename) is None or tools.getFileType(filename) == "":
+                possibleExtensions = [".png", ".bmp", ".gif", ".jpg", ".pdf"]
+                possibleExtensions = possibleExtensions + [x.upper() for x in possibleExtensions]
+                found = False
+                for fileEnding in possibleExtensions:
+                    if os.path.isfile(filename + fileEnding):
+                        logging.info("Found a file named " + filename + fileEnding + ", will assume that this file is meant by")
+                        logging.info(l.replace("\n", ""))
+                        filename = filename + fileEnding
+                        found = True
+                        break
+                if not found:
+                    logging.info("Didn't find a file with this name, ignoring this dependency.")
             starts.append(filename)
     if len(starts) == 0:
         return None
