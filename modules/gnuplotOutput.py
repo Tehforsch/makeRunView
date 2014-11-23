7e505ebd7bfd2d37a517b8c8c0ad6cbe643099bb
Tehforsch <Tonipeter92@googlemail.com>
2014-11-23 13:34:34 +0100

    "Safety measure added for gnuplot files with dynamic output"

--- modules/gnuplotOutput.py
+++ modules/gnuplotOutput.py
@@ -8,7 +8,13 @@ def check(f, lines):
     targets = []
     for l in lines:
         if "set output" in l:
-            targets.append(tools.charactersBetween(l, "\"", "\""))
+            if l.count("\"") == 2:
+                targets.append(tools.charactersBetween(l, "\"", "\""))
+            elif l.count("\'") == 2:
+                targets.append(tools.charactersBetween(l, "\'", "\'"))
+            else:
+                logging.info("Dangerous gnuplot output line, not adding dependency:")
+                logging.info(l)
     if len(targets) == 0:
         return None
     return Dependency(starts = start, targets = targets, command = "gnuplot", printOutput = True)
