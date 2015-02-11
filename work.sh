7a3ea71ce077b3fca704389d83be334f088f8869
Tehforsch <Tonipeter92@googlemail.com>
2015-02-11 17:46:57 +0100

    Changed the project structure to make makeRunView a package, and overall just look better

--- work.sh
+++
@@ -1,2 +0,0 @@
-# WE META NOW
-when-changed modules/pymodule.py modules/texmodule.py modules/gpimodule.py dependencymanager.py config.py dependency.py executor.py filestate.py main.py makerunview.py observer.py tools.py utils/osUtils.py utils/fileUtils.py -c "sh ./killAndStart.sh"
