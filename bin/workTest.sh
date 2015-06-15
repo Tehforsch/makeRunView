5b46a7ce1c60a454ed466a39fe84a6817405819b
Tehforsch <Tonipeter92@googlemail.com>
2015-06-15 13:46:41 +0200

    Added coverage check and fixed the test run script

--- bin/workTest.sh
+++ bin/workTest.sh
@@ -1,3 +1,3 @@
 # WE META NOW
 echo $(pwd)
-when-changed modules/pymodule.py modules/texmodule.py modules/gpimodule.py makeRunView/dependencymanager.py makeRunView/config.py makeRunView/dependency.py makeRunView/executor.py makeRunView/filestate.py makeRunView/main.py makeRunView/makerunview.py makeRunView/observer.py makeRunView/tools.py makeRunView/utils/osUtils.py makeRunView/utils/fileUtils.py test/testtools.py test/testmakerunview.py -c "test"
+when-changed modules/pymodule.py modules/texmodule.py modules/gpimodule.py makeRunView/dependencymanager.py makeRunView/config.py makeRunView/dependency.py makeRunView/executor.py makeRunView/filestate.py makeRunView/main.py makeRunView/makerunview.py makeRunView/observer.py makeRunView/tools.py makeRunView/utils/osUtils.py makeRunView/utils/fileUtils.py test/testtools.py test/testmakerunview.py -c "nosetests --with-coverage"
