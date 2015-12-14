c4828c0974a37ddfffde41035343cf8e570f523c
Tehforsch <Tonipeter92@googlemail.com>
2015-12-14 15:15:42 +0100

    python3.4 -> python3.5

--- bin/killAndStart.sh
+++ bin/killAndStart.sh
@@ -2,4 +2,4 @@ clear
 # Check if the python script was already running, terminate it
 processId=$(ps axf | ack python3.4 | ack main.py | ack example | ack -v ack | ack  "^([\s]*[\d]*)"  --output=\$1)
 kill $processId > /dev/null 2>&1
-python3.4 -B makeRunView/main.py -v example & 
+python3.5 -B makeRunView/main.py -v example & 
